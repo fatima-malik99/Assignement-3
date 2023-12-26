54682999097a859a851ffe4d2a837616c4d6b13a
fatima-malik99 <7223fatimamalik@gmail.com>
2023-12-26 10:37:05 +0500

    php assignment

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+ $Numone = 12;
+ $Numtwo = 40;
+ $Numthree = 7;
+ 
+ if($Numone>$Numtwo && $Numone>$Numthree){
+	echo "12 is the greater number";}
+	
+ else if($Numtwo>$Numone && $Numtwo>$Numthree){
+  echo "40 is the greatest number";}
+  
+ else{
+   echo "7 is the greater number";}
+?>
